c7b2b23bc634efa753455d9450a56e5a916cbdcb
vivek <guptavivek1035@gmail.com>
2023-10-29 21:43:32 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
-import { useEffect, useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Color, setColor] = useState("slate");
 
-  const colors = ["green", "blue", "white", "orange", "violet", "yellow", "red", "gray"]
+  const colors = [
+    "green",
+    "blue",
+    "white",
+    "orange",
+    "violet",
+    "yellow",
+    "red",
+    "gray",
+  ];
 
-  useEffect(() => {
-    document.addEventListener('click', (e) => {
-      console.log(e.target)
-    })
-  }, [])  
   return (
     <>
-      <div className='bg-slate-900 h-screen w-full'>
-        <div className='h-10 ml-10 mr-10 w-[-webkit-fill-available] p-7 pt-8 pb-7 bg-white rounded-xl fixed bottom-[15%] flex items-center justify-around' id="buttons">
-          {
-            colors.map((item, index) => 
-              (
-                <button key={index} className={`bg-${item}-400 p-1 rounded-xl ring-2 hover:ring-${item}-800 min-w-[7em]`}>{item}</button>
-              )
-            )
-          }
+      <div className={`bg-${Color}-900 h-screen w-full duration-500`} id="mainbg">
+        <div
+          className="h-10 ml-10 mr-10 w-[-webkit-fill-available] p-7 pt-8 pb-7 bg-white rounded-xl fixed bottom-[15%] flex items-center justify-around"
+          id="buttons"
+        >
+          {colors.map((item, index) => (
+            <button
+              key={index}
+              className={`bg-${item}-400 m-1 p-1 rounded-xl ring-2 hover:ring-${item}-800 w-[7em]`}
+              id={`${item}`}
+              onClick={() => setColor(item)}
+            >
+              {item}
+            </button>
+          ))}
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
