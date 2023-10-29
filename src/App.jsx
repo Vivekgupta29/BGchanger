0ca69c66db30c72f17c06a19aafc279578ebb45a
vivekshop <guptavivek1035@gmail.com>
2023-10-29 19:33:23 +0530

    trying to solve button

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const colors = ["green", "blue", "white", "orange", "violet", "yellow", "red", "gray"]
+
+  useEffect(() => {
+    document.addEventListener('click', (e) => {
+      console.log(e.target)
+    })
+  }, [])  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='bg-slate-900 h-screen w-full'>
+        <div className='h-10 ml-10 mr-10 w-[-webkit-fill-available] p-7 pt-8 pb-7 bg-white rounded-xl fixed bottom-[15%] flex items-center justify-around' id="buttons">
+          {
+            colors.map((item, index) => 
+              (
+                <button key={index} className={`bg-${item}-400 p-1 rounded-xl ring-2 hover:ring-${item}-800 min-w-[7em]`}>{item}</button>
+              )
+            )
+          }
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
